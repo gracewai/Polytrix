091e87a4a453d319dbb0ccf65336e24bfd40af43
Kristopher Lam <kris@sandb0x.io>
2015-01-05 23:41:44 +0800

    User logout implementation

---
+++ client/app/scripts/services/upload.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name clientApp.upload
+ * @description
+ * # upload
+ * Service in the clientApp.
+ */
+angular.module('clientApp')
+  .service('upload', function () {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
